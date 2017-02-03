a44805b6309ed9fd3724edffab54a9539dca04df
AlenPelin <AlenPelin@gmail.com>
2017-02-03 12:51:49 +1100

    Import code

---
+++ src/Sitecore.Support.108514/Form/Core/Pipelines/ProcessMessage/VerifyPageMode.cs
@@ -0,0 +1,21 @@
+﻿namespace Sitecore.Support.Form.Core.Pipelines.ProcessMessage
+{
+  using Sitecore.Diagnostics;                           
+  using Sitecore.WFFM.Abstractions.Mail;
+
+  public class VerifyPageMode
+  {
+    [UsedImplicitly]
+    public void Process(ProcessMessageArgs args)
+    {
+      if (Context.PageMode.IsNormal)
+      {
+        return;
+      }
+
+      Log.Info($"Sending message was aborted due to preview mode (Sitecore.Support.108514)", this);
+
+      args.AbortPipeline();
+    }
+  }
+}
---
+++ src/Sitecore.Support.108514/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.108514")]
+[assembly: AssemblyProduct("Sitecore.Support.108514")]
+[assembly: ComVisible(false)]
